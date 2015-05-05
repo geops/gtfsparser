0fbf6614a59c6f5ccd8136dcf4519c2f97a346d4
Patrick Brosi <info@patrickbrosi.de>
2015-05-06 01:06:51 +0200

    add fare parsing, add shape struct

---
+++ gtfs/fareattributes.go
@@ -0,0 +1,24 @@
+// Copyright 2015 geOps
+// Authors: patrick.brosi@geops.de
+//
+// Use of this source code is governed by a GPL v2
+// license that can be found in the LICENSE file
+
+package gtfs
+
+type FareAttribute struct {
+	Id       string
+	Price     string
+	Currency_type      string
+	Payment_method int
+	Transfers     int
+	Transfer_duration    int
+	Rules           []*FareAttributeRule
+}
+
+type FareAttributeRule struct {
+	Route *Route
+	Origin_id string  		// connection to Zone_id in Stop
+	Destination_id string 	// connection to Zone_id in Stop
+	Contains_id string  	// connection to Zone_id in Stop
+}
---
+++ gtfs/shape.go
@@ -0,0 +1,19 @@
+// Copyright 2015 geOps
+// Authors: patrick.brosi@geops.de
+//
+// Use of this source code is governed by a GPL v2
+// license that can be found in the LICENSE file
+
+package gtfs
+
+type Shape struct {
+	Id                  string
+	Points				[]*ShapePoint
+}
+
+type ShapePoint struct {
+	Lat                 float32
+	Lon                 float32
+	Sequence 			int
+	Dist_traveled		float32
+}
